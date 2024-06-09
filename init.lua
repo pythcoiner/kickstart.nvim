570e1f7327575f0b26496f0b772315bae2053763
pythcoiner <pythcoiner@proton.me>
2024-06-09 06:55:33 +0200

    add next/previous hunk binding

--- init.lua
+++ init.lua
@@ -187,6 +187,9 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+vim.keymap.set('n', '<leader>cj', ':Gitsigns next_hunk<CR>', { desc = 'Next hunk' })
+vim.keymap.set('n', '<leader>ck', ':Gitsigns prev_hunk<CR>', { desc = 'Previous hunk' })
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
