f8a69b64e9e6333a0194a21614a8eaa6dbe21de6
pythcoiner <pythcoiner@proton.me>
2024-07-04 19:03:25 +0200

    file overview

--- init.lua
+++ init.lua
@@ -334,6 +334,9 @@ vim.keymap.set('n', '<leader>dd', DiagramMode, { desc = 'Disable diagram mode' }
 -- Map the function to a key combination in visual block mode
 vim.api.nvim_set_keymap('x', '<Leader>r', ':lua ReplaceWithSpaces()<CR>', { noremap = true, silent = true })
 
+-- Toggle file overview
+vim.keymap.set('n', '<leader>ll', ':SymbolsOutline<CR>', { desc = 'File Overview' })
+
 -- [[ NVimTree keymaps ]]
 vim.api.nvim_set_keymap('n', '<leader>aa', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
 
@@ -424,6 +427,62 @@ require('lazy').setup({
 
   -- Ascii diagram
   'pythcoiner/venn.nvim',
+
+  -- Code structure overview
+  {
+    'simrat39/symbols-outline.nvim',
+    config = function()
+      require('symbols-outline').setup {
+        highlight_hovered_item = true,
+        show_guides = true,
+        auto_preview = false,
+        position = 'right',
+        width = 25,
+        show_numbers = false,
+        show_relative_numbers = false,
+        show_symbol_details = true,
+        preview_bg_highlight = 'Pmenu',
+        keymaps = {
+          close = { '<Esc>', 'q' },
+          goto_location = '<Cr>',
+          focus_location = 'o',
+          hover_symbol = '<C-space>',
+          toggle_preview = 'K',
+          rename_symbol = 'r',
+          code_actions = 'a',
+        },
+        lsp_blacklist = {},
+        symbol_blacklist = {},
+        symbols = {
+          File = { icon = '', hl = 'TSURI' },
+          Module = { icon = '', hl = 'TSNamespace' },
+          Namespace = { icon = '', hl = 'TSNamespace' },
+          Package = { icon = '', hl = 'TSNamespace' },
+          Class = { icon = '', hl = 'TSType' },
+          Method = { icon = '', hl = 'TSMethod' },
+          Property = { icon = '', hl = 'TSMethod' },
+          Field = { icon = '', hl = 'TSField' },
+          Constructor = { icon = '', hl = 'TSConstructor' },
+          Enum = { icon = '', hl = 'TSType' },
+          Interface = { icon = '', hl = 'TSType' },
+          Function = { icon = '', hl = 'TSFunction' },
+          Variable = { icon = '', hl = 'TSConstant' },
+          Constant = { icon = 'C', hl = 'TSConstant' },
+          String = { icon = 'S', hl = 'TSString' },
+          Number = { icon = '', hl = 'TSNumber' },
+          Boolean = { icon = '◩', hl = 'TSBoolean' },
+          Array = { icon = '', hl = 'TSConstant' },
+          Object = { icon = '', hl = 'TSType' },
+          Key = { icon = 'K', hl = 'TSType' },
+          Null = { icon = '-', hl = 'TSType' },
+          EnumMember = { icon = '', hl = 'TSField' },
+          Struct = { icon = '', hl = 'TSType' },
+          Event = { icon = '', hl = 'TSType' },
+          Operator = { icon = '+', hl = 'TSOperator' },
+        },
+      }
+    end,
+  },
 
   -- nvim-tree
   {
