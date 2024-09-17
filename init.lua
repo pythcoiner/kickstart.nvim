7bdd78fc634c45ae84b9bb0002e98fa517af9e5e
pythcoiner <pythcoiner@proton.me>
2024-09-17 09:02:00 +0200

    force encoding to utf-8

--- init.lua
+++ init.lua
@@ -6,6 +6,11 @@ vim.o.history = 1000
 -- 1 = less, 15 = more
 vim.o.verbose = 0
 
+-- force utf-8 encoding
+-- to avoid save error w/ diagrams
+vim.opt.fileencoding = 'utf-8'
+vim.opt.encoding = 'utf-8'
+
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
