fe240f3173e0a08990e6aadf339a5d0c34725d46
pythcoiner <pythcoiner@proton.me>
2025-08-18 03:30:40 +0200

    add <leader>ga for show documentation

--- init.lua
+++ init.lua
@@ -1048,7 +1048,7 @@ require('lazy').setup({
 
           -- Opens a popup that displays documentation about the word under your cursor
           --  See `:help K` for why this keymap.
-          -- map('K', vim.lsp.buf.hover, 'Hover Documentation')
+          map('ga', vim.lsp.buf.hover, 'Hover Documentation')
 
           -- WARN: This is not Goto Definition, this is Goto Declaration.
           --  For example, in C this would take you to the header.
